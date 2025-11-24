0a5943dfeaf50f0210150c1c27eb76b760589df8
Khason Murphy <khason.t@gmail.com>
2025-11-24 17:55:50 -0500

    near complete
    Missing database connection

--- src/ui/Customer/src/main/java/com/demo/customer/LoginApplication.java
+++ src/ui/Customer/src/main/java/com/demo/customer/LoginApplication.java
@@ -11,8 +11,8 @@ public class LoginApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("Login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Mom & Pop's Pizzaria");
         stage.setScene(scene);
         stage.show();
     }
