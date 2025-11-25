dad294da05447355c0431d5ed8983b1e1123fa80
Khason Murphy <khason.t@gmail.com>
2025-11-24 20:49:17 -0500

    small changes

--- src/ui/Customer/src/main/java/com/demo/customer/homepageController.java
+++ src/ui/Customer/src/main/java/com/demo/customer/homepageController.java
@@ -32,6 +32,8 @@ public class homepageController {
     private Button visitSpecialsPage;
     @FXML
     private Button vistitOrderPage;
+    @FXML
+    private Button visitPreviousPage;
 
 
     @FXML
